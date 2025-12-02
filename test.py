198bee100a26378798fe9035607fa58d6d2ab07f
sam-575 <korovinu@gmail.com>
2025-12-02 14:13:47 +0300

    Chenge in file - test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
 
+print('Enter word "stop" for result')
+summ = 0
+while True:
+    x = input('Enter num: ')
+    if x == 'stop':
+        break
+    x = int(x)
+    summ += x
+print('Summ number:', summ)
+input()
